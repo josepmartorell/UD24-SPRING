b98f346596541a26616e434e87cd445f10647598
josepmartorell <martorelljosep>
2022-08-13 13:14:36 +0200

    start project

---
+++ UD24-CRUD-H2/src/main/java/com/crud/h2/dao/IClienteDAO.java
@@ -0,0 +1,16 @@
+package com.crud.h2.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.crud.h2.dto.Cliente;
+
+/**
+ * @author jtech
+ *
+ */
+public interface IClienteDAO extends JpaRepository<Cliente, Long>{
+	
+	//Listar clientes or campo nombre
+	public List<Cliente> findByNombre(String nombre);
+}
